ba0ab3308c4bf8574671a2252328d8dda803ee25
limon <mhlimon571@gmail.com>
2025-07-30 01:32:40 +0600

    add inventory app

--- Eshop/settings.py
+++ Eshop/settings.py
@@ -29,6 +29,7 @@ INSTALLED_APPS = [
 
     'store',
     'order',
+    'InventoryLog',
 ]
 
 MIDDLEWARE = [
--- Eshop/urls.py
+++ Eshop/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('store.urls')),
     path('order/',include('order.urls')),
+    path('inventory/',include('InventoryLog.urls')),
 ]
