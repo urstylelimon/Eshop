bd77eacc92d4e1d0db0de2d4256a646e04db8566
limon <mhlimon571@gmail.com>
2025-07-30 01:32:12 +0600

    add inventory model

--- store/models.py
+++ store/models.py
@@ -15,3 +15,4 @@ class Product (models.Model):
     description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
     image = models.ImageField(upload_to = 'products/')
+
