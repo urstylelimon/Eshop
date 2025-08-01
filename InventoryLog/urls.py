7687bd80fff7d55ff3119bba7bc4a545b3ef664c
limon <mhlimon571@gmail.com>
2025-08-01 21:57:05 +0600

    add inventory url

--- InventoryLog/urls.py
+++ InventoryLog/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.inventory_home, name='inventory_home'),
+    path('update_inventory/<int:id>', views.update_inventory, name='update_inventory'),
 ]
