92cf2df93f68a86e3d63b1d3cecc4e72e9c9e3e2
limon <mhlimon571@gmail.com>
2025-10-09 00:19:16 +0600

    add to cart bug fixed

--- store/urls.py
+++ store/urls.py
@@ -3,9 +3,9 @@ from . import views
 
 urlpatterns = [
     path('',views.home,name='home'),
-    path('single_product/<int:pk>',views.single_product,name='single_product'),
+    path('single_product/<int:pk>/',views.single_product,name='single_product'),
     # path('cart_view/',views.cart_view,name='cart_view'),
-    path('add_to_cart/<int:pk>',views.add_to_cart,name='add_to_cart'),
+    path('add_to_cart/<int:pk>/',views.add_to_cart,name='add_to_cart'),
 
 ]
 
