77c4405db31771db82622f66041821f87d8e06b7
limon <mhlimon571@gmail.com>
2025-10-14 23:31:03 +0600

    add more images in single products page up to 3

--- store/models.py
+++ store/models.py
@@ -14,5 +14,11 @@ class Product (models.Model):
     slug = models.SlugField(max_length = 100)
     description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
+
+    #1. This is the main Image
     image = models.ImageField(upload_to = 'products/')
 
+    # 2. Add these three new optional image fields
+    image2 = models.ImageField(upload_to='products/', blank=True, null=True)
+    image3 = models.ImageField(upload_to='products/', blank=True, null=True)
+    image4 = models.ImageField(upload_to='products/', blank=True, null=True)
