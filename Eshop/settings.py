e4a1815be913e1e9acfffbb2033d07533e9c09cb
Mh Limon <mhlimon571@gmail.com>
2025-11-12 16:52:42 +0600

    add env

--- Eshop/settings.py
+++ Eshop/settings.py
@@ -1,5 +1,11 @@
 import os
 from pathlib import Path
+from dotenv import load_dotenv
+
+# Load environment variables from .env file
+load_dotenv()
+
+META_CAPI_TOKEN = os.environ.get('META_CAPI_TOKEN')
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -14,7 +20,7 @@ SECRET_KEY = 'django-insecure-n01!kv!_xb(k-1vx1a^6*no#+#%s=by#)(bg+ysn4=%g+-$ds+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['192.168.0.200', '127.0.0.1', '192.168.100.40','192.168.0.185']
+ALLOWED_HOSTS = ['192.168.0.200', '127.0.0.1', '192.168.100.110','192.168.0.185']
 
 
 # Application definition
